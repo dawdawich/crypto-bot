920f5b1d8f6d5fd2ca3ed7e3002da29344e951e2
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2023-12-22 18:02:52 +0200

    Refactor ManagersPage UI rendering
    The output of the ManagersPage component has been refactored to improve readability and maintainability. The table displaying the managers data has been moved into a separate component. This change simplifies the return statement and makes it clear when the table should be rendered.

--- crypto-bot-ui/src/pages/manager/ManagersPage.tsx
+++ crypto-bot-ui/src/pages/manager/ManagersPage.tsx
@@ -43,37 +43,40 @@ const ManagersPage: React.FC = () => {
         </div>
     );
 
+
+    const dataTable = <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Choose Strategy</TableCell>
+                    <TableCell>Custom Analyzer Id</TableCell>
+                    <TableCell>Status</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {data.map(manager => (
+                    <TableRow
+                        key={manager.id}
+                        sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                        onClick={() => setLocation(`/manager/${manager.id}`)}
+                    >
+                        <TableCell component="th" scope="row">{manager.id}</TableCell>
+                        <TableCell align="left">{manager.chooseStrategy}</TableCell>
+                        <TableCell align="left">{manager.customAnalyzerId}</TableCell>
+                        <TableCell align="left">{manager.active ? "Active" : "Disabled"}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </TableContainer>;
+
     return (
         <div>
             <h1>Managers Page</h1>
             <button className="material-button" onClick={() => setIsCreateManagerDialogOpen(true)}>Add Analyzer</button>
-            <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Id</TableCell>
-                            <TableCell>Choose Strategy</TableCell>
-                            <TableCell>Custom Analyzer Id</TableCell>
-                            <TableCell>Status</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {data.map(manager => (
-                            <TableRow
-                                key={manager.id}
-                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                                onClick={() => setLocation(`/manager/${manager.id}`)}
-                            >
-                                <TableCell component="th" scope="row">{manager.id}</TableCell>
-                                <TableCell align="left">{manager.chooseStrategy}</TableCell>
-                                <TableCell align="left">{manager.customAnalyzerId}</TableCell>
-                                <TableCell align="left">{manager.active ? "Active" : "Disabled"}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
             <CreateManagerDialog open={isCreateManagerDialogOpen} onClose={() => setIsCreateManagerDialogOpen(false)} onCreate={createManagerAndUpdate} />
+            {data.length === 0 ? null : dataTable}
         </div>
     );
 }
