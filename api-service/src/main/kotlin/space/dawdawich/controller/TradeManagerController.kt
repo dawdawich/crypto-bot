40c1a9580104ffb6f5f4d72a36ba7baef60b89ff
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2023-12-23 17:26:01 +0200

    Revise parameter name in TradeManagerService methods
    The parameter name 'id' in the methods 'updateTradeManagerStatus' and 'findManager' of TradeManagerService.kt is changed to 'managerId' to enhance code readability. It provides clearer context within the method's function, and aligns with the parameter name used in the respective repository methods.

--- api-service/src/main/kotlin/space/dawdawich/controller/TradeManagerController.kt
+++ api-service/src/main/kotlin/space/dawdawich/controller/TradeManagerController.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
+import space.dawdawich.controller.model.ActivationRequest
 import space.dawdawich.controller.model.TradeManagerRequest
 import space.dawdawich.controller.model.TradeManagerResponse.Companion.convert
 import space.dawdawich.repositories.entity.TradeManagerDocument
@@ -29,7 +30,7 @@ class TradeManagerController(private val tradeMangerService: TradeManagerService
 
     @PutMapping("/{managerId}/status")
     @ResponseStatus(HttpStatus.OK)
-    fun changeManagerStatus(user: Authentication, @PathVariable managerId: String, @RequestBody status: Boolean) = tradeMangerService.updateTradeManagerStatus(managerId, user.name, status)
+    fun changeManagerStatus(user: Authentication, @PathVariable managerId: String, @RequestBody request: ActivationRequest) = tradeMangerService.updateTradeManagerStatus(managerId, user.name, request.status)
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
