b9eb94b3097169530fcbef8975a2eae2026a66a0
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2023-12-30 13:11:37 +0200

    Refactor MainHeader and update CSS
    Significant changes were made to the MainHeader component and corresponding CSS. Toolbars, buttons, and logo image spots were added to enhance the navigation experience. The CSS was also refactored with new class definitions to accommodate the changes in the MainHeader component. Overall, these changes aim to improve layout, navigation, and the visual experience of the application.

--- crypto-bot-ui/src/pages/MainHeader.tsx
+++ crypto-bot-ui/src/pages/MainHeader.tsx
@@ -1,35 +1,48 @@
-import {Link} from "wouter";
-import '../css/HeaderLinkBox.css';
-import '../css/CuteLink.css';
+import {useLocation} from "wouter";
+import {AppBar, Box, Button, styled, Toolbar} from "@mui/material";
+
+const StyledToolbar = styled(Toolbar)({
+    justifyContent: 'space-between',
+    gap: '10px',
+});
 
 const MainHeader = () => {
     let userData = localStorage.getItem('auth.token');
+    const [, navigate] = useLocation();
 
-    if (!userData) {
-        return (
-            <div className='links-box'>
-                <div className='links-box-item'><Link to={"/login"} className='cute-link'>Login</Link></div>
-                <div className='links-box-item'><Link to={"/signup"} className='cute-link'>Sign Up</Link></div>
-            </div>
-        )
-    }
+    // if (!userData) {
+    //     return (
+    //         <div className='links-box'>
+    //             <div className='links-box-item'><Link to={"/login"} className='cute-link'>Login</Link></div>
+    //             <div className='links-box-item'><Link to={"/signup"} className='cute-link'>Sign Up</Link></div>
+    //         </div>
+    //     )
+    // }
 
     const role = localStorage.getItem('auth.role')
 
     return (
-        <div className='links-box'>
-            <div className='links-box-item'><Link to={"/analyzer"} className='cute-link'>Analyzers List</Link></div>
-            <div className='links-box-item'><Link to={"/top-analyzers"} className='cute-link'>Public Top Analyzers</Link></div>
-            <div className='links-box-item'><Link to={"/account"} className='cute-link'>Account</Link></div>
-            <div className='links-box-item'><Link to={"/manager"} className='cute-link'>Managers List</Link></div>
-            {
-                role === 'ADMIN' &&
-                <div className='links-box-item'>
-                    <Link to={"/symbols"} className='cute-link'>Symbols</Link>
-                </div>
-            }
-        </div>
-    )
+        <AppBar position="static" color="default" elevation={1}>
+            <StyledToolbar>
+                <Box display="flex" alignItems="center">
+                    <img src="" alt="Logo" style={{ height: 50 }} />
+                </Box>
+                <Box display="flex" gap={2}>
+                    <Button color="inherit" onClick={() => navigate("/analyzer")}>Analyzers</Button>
+                    <Button color="inherit" onClick={() => navigate("/top-analyzers")}>Public Top Analyzers</Button>
+                    <Button color="inherit" onClick={() => navigate("/manager")}>Managers</Button>
+                    <Button color="inherit" onClick={() => navigate("/account")}>Account</Button>
+                    {
+                        role === 'ADMIN' &&
+                        <Button color="inherit" onClick={() => navigate("/symbols")}>Symbols</Button>
+                    }
+                </Box>
+                <Button variant="outlined" color="primary">
+                    Connect Wallet
+                </Button>
+            </StyledToolbar>
+        </AppBar>
+    );
 }
 
 export default MainHeader;
