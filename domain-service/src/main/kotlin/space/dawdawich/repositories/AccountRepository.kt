c461e60b183e1bed2d101d764ea34ff731a1dfe2
oleksandr.hnatiuk <oleksandr.hnatiuk@pm.bet>
2023-12-30 19:40:13 +0200

    CBA-7 - Add validation to already registered user

--- domain-service/src/main/kotlin/space/dawdawich/repositories/AccountRepository.kt
+++ domain-service/src/main/kotlin/space/dawdawich/repositories/AccountRepository.kt
@@ -11,6 +11,8 @@ interface AccountRepository : MongoRepository<AccountDocument, String> {
 
     fun findByEmail(email: String): AccountDocument?
 
+    fun existsByEmailAndUsername(email: String, username: String): Boolean
+
     @Query("{email:  ?0}")
     @Update("{\$set: { username: ?1, name: ?2, surname: ?3, password: ?4, createTime: ?5, updateTime: ?6, role: ?7 }}")
     fun fillAccountInfo(email: String, username: String, name: String, surname: String, password: String, createTime: Long = System.currentTimeMillis(), updateTime: Long = createTime, role: String = "USER")
