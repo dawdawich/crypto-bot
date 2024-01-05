32b0c78b0411eebc5138094572f433955b825b8b
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-01-05 12:05:01 +0200

    Added KafkaConfiguration class in analyzer-service
    The new class, KafkaConfiguration, has been added in the analyzer-service module. This configuration class sets up Kafka's behavior in our application, including defining the consumer factory and KafkaListenerContainerFactory instances.

---
+++ analyzer-service/src/main/kotlin/space/dawdawich/configuration/KafkaConfiguration.kt
@@ -0,0 +1,30 @@
+package space.dawdawich.configuration
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import space.dawdawich.repositories.entity.TradeManagerDocument
+
+@Configuration
+open class KafkaConfiguration {
+
+    @Bean
+    open fun kafkaListenerContainerFactory(consumerFactory: ConsumerFactory<String, String>) =
+        ConcurrentKafkaListenerContainerFactory<String, String>().apply { this.consumerFactory = consumerFactory }
+
+    @Bean
+    open fun consumerFactory(@Value("\${spring.kafka.bootstrap-servers}") bootstrapServer: String): ConsumerFactory<String, String> {
+        val configProps: MutableMap<String, Any> = HashMap()
+        configProps[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServer
+        configProps[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configProps[ConsumerConfig.GROUP_ID_CONFIG] = "ticker_group"
+        return DefaultKafkaConsumerFactory(configProps)
+    }
+}
