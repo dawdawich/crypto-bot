1d870518a6b9d75c7be86e309f530d127f09c4ed
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-02-05 17:46:49 +0200

    Enable scheduling in EventListenerApplication
    The annotation `@EnableScheduling` has been added to the EventListenerApplication class. It allows the scheduling of tasks to be enabled, which is particularly beneficial for applications that require regular non-blocking tasks or the execution of specific methods at particular times or intervals.

--- event-listener/src/main/kotlin/space/dawdawich/EventListenerApplication.kt
+++ event-listener/src/main/kotlin/space/dawdawich/EventListenerApplication.kt
@@ -2,7 +2,9 @@ package space.dawdawich
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class EventListenerApplication
 
