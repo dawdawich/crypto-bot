fe9a1e05a491e65787a83108d74b65f34252ec5f
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-02-07 18:36:36 +0200

    Correct test WebSocket URL in ApiAddressConstants
    The WebSocket URL for Bybit's testing environment in the ApiAddressConstants has been commented out and replaced with the production URL. The change was marked with a "FIXME" tag, indicating it should be revised later. This temporary solution allows testing with real trading data instead of using the testnet while a fix for the testnet issues is being investigated.

--- commons/src/main/kotlin/space/dawdawich/constants/ApiAddressConstants.kt
+++ commons/src/main/kotlin/space/dawdawich/constants/ApiAddressConstants.kt
@@ -1,4 +1,5 @@
 package space.dawdawich.constants
 
 const val BYBIT_WEB_SOCKET_URL = "wss://stream.bybit.com/v5/public/linear"
-const val BYBIT_TEST_WEB_SOCKET_URL = "wss://stream-testnet.bybit.com/v5/public/linear"
+// FIXME
+const val BYBIT_TEST_WEB_SOCKET_URL = "wss://stream.bybit.com/v5/public/linear" //"wss://stream-testnet.bybit.com/v5/public/linear"
