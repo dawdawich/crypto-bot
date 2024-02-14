210ede99fb7378543b48cbf37e51ef000edb0dbe
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-02-14 23:37:27 +0200

    Add logging to PriceTickerListener in AnalyzerService
    Logging functionality has been introduced to the PriceTickerListener class within the AnalyzerService. This helps to track the processing time for price updates, providing visibility into the time it takes to process each price update. The log captures details like symbol, topic, and elapsed time for each process.

--- analyzer-service/src/main/kotlin/space/dawdawich/services/PriceTickerListener.kt
+++ analyzer-service/src/main/kotlin/space/dawdawich/services/PriceTickerListener.kt
@@ -2,22 +2,25 @@ package space.dawdawich.services
 
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import org.springframework.kafka.listener.AcknowledgingMessageListener
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
 import org.springframework.kafka.listener.ContainerProperties
 import org.springframework.kafka.listener.MessageListener
 
 class PriceTickerListener(private val kafkaContainer: ConcurrentMessageListenerContainer<String, String>) {
+    private val log = KotlinLogging.logger {  }
     private val observers = mutableListOf<(Double) -> Unit>()
-
 
     init {
         kafkaContainer.setupMessageListener(AcknowledgingMessageListener<String, String> { record, acknowledge ->
+            val startTime = System.currentTimeMillis()
             val newPrice = record.value().toDouble()
             acknowledge?.acknowledge()
             runBlocking {
                 observers.forEach { execute -> launch { execute(newPrice) } }
             }
+            log.info { "Processed price update for symbol '${record.key()}'; topic '${record.topic()}'; time elapsed - '${System.currentTimeMillis() - startTime}'" }
         })
         kafkaContainer.start()
         Runtime.getRuntime().addShutdownHook(Thread {
