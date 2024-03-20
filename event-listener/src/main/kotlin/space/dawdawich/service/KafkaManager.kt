db29d778ed4ce7be27bdd3cb8797e57f3bfad47f
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-03-20 23:09:34 +0200

    Add throttling to KafkaManager's sendTickerEvent
    Enhanced the sendTickerEvent method in KafkaManager. Now it limits the send operation to once every 5 seconds for each unique topic and partition pair. This change prevents excessive message sending by implementing a rate limit for each topic and partition. This should improve performance and efficiency.

--- event-listener/src/main/kotlin/space/dawdawich/service/KafkaManager.kt
+++ event-listener/src/main/kotlin/space/dawdawich/service/KafkaManager.kt
@@ -2,7 +2,7 @@ package space.dawdawich.service
 
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
-import space.dawdawich.constants.BYBIT_TICKER_TOPIC
+import kotlin.time.Duration.Companion.seconds
 
 @Service
 class KafkaManager(private val kafkaTemplate: KafkaTemplate<String, String>) {
@@ -10,7 +10,10 @@ class KafkaManager(private val kafkaTemplate: KafkaTemplate<String, String>) {
     val lastUpdateTopicTime: MutableMap<String, Long> = mutableMapOf()
 
     fun sendTickerEvent(topic: String, symbol: String, partition: Int, value: String) {
-        lastUpdateTopicTime["$topic-$partition"] = System.currentTimeMillis()
-        kafkaTemplate.send(topic, partition, symbol, value)
+        val currentTime = System.currentTimeMillis()
+        if ((currentTime - (lastUpdateTopicTime["$topic-$partition"] ?: 0)) > 5.seconds.inWholeMilliseconds) {
+            lastUpdateTopicTime["$topic-$partition"] = System.currentTimeMillis()
+            kafkaTemplate.send(topic, partition, symbol, value)
+        }
     }
 }
