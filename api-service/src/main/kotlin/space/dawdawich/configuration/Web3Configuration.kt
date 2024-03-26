a0a0d3ff3b6e85ed3cc01e713255888dd7fc0726
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-03-26 17:46:18 +0200

    Refactor Web3Configuration for flexible blockchain addresses
    The Web3Configuration has been refactored to dynamically assign a blockchain address for the web3Client depending on the environment. This allows for more flexibility, enabling different blockchain networks for different environments (local, prod) without having to manually change the addresses in code.

--- api-service/src/main/kotlin/space/dawdawich/configuration/Web3Configuration.kt
+++ api-service/src/main/kotlin/space/dawdawich/configuration/Web3Configuration.kt
@@ -1,19 +1,14 @@
 package space.dawdawich.configuration
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.http.HttpService
 
 @Configuration
-class Web3Configuration {
+class Web3Configuration(@Value("\${app.blockchain-address}") private val blockchainAddress: String) {
 
     @Bean
-    @Profile("!prod")
-    fun testWeb3Client() = Web3j.build(HttpService("https://sepolia.infura.io/v3/833bc9ae0e7a4d899cb8a78de45081ed"))!!
-
-    @Bean
-    @Profile("prod")
-    fun web3Client() = Web3j.build(HttpService("https://mainnet.infura.io/v3/833bc9ae0e7a4d899cb8a78de45081ed"))!!
+    fun web3Client() = Web3j.build(HttpService(blockchainAddress))!!
 }
