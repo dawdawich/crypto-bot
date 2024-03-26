2a9b5c0d21a1e7c249a603e7f61f7f2c4860ff97
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-03-26 17:23:52 +0200

    Configure environment-specific Web3 clients
    The Web3Configuration now provides a different web3 client for test and production profiles. This gives more flexibility to use different blockchain networks for development and production. The change also includes updated local server URLs in the .env.development file, ensuring correct paths without hardcoded API routes.

--- api-service/src/main/kotlin/space/dawdawich/configuration/Web3Configuration.kt
+++ api-service/src/main/kotlin/space/dawdawich/configuration/Web3Configuration.kt
@@ -2,6 +2,7 @@ package space.dawdawich.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.http.HttpService
 
@@ -9,5 +10,10 @@ import org.web3j.protocol.http.HttpService
 class Web3Configuration {
 
     @Bean
-    fun web3Client() = Web3j.build(HttpService("https://sepolia.infura.io/v3/833bc9ae0e7a4d899cb8a78de45081ed"))
+    @Profile("!prod")
+    fun testWeb3Client() = Web3j.build(HttpService("https://sepolia.infura.io/v3/833bc9ae0e7a4d899cb8a78de45081ed"))!!
+
+    @Bean
+    @Profile("prod")
+    fun web3Client() = Web3j.build(HttpService("https://mainnet.infura.io/v3/833bc9ae0e7a4d899cb8a78de45081ed"))!!
 }
