cf1ce116323f91a4f2057e86126835887c9e9904
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-07-14 21:27:10 +0300

    Add logging to order placement
    KotlinLogging.logger was added to ManagerUtils.kt to provide logging capabilities. The current setup logs information when an attempt is made to place an order, detailing the order's symbol, quantity, and price. This improves tracking and debugging for order placements.

--- trade-manager/src/main/kotlin/space/dawdawich/managers/ManagerUtils.kt
+++ trade-manager/src/main/kotlin/space/dawdawich/managers/ManagerUtils.kt
@@ -1,6 +1,7 @@
 package space.dawdawich.managers
 
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import space.dawdawich.integration.client.PrivateHttpClient
 import space.dawdawich.model.strategy.StrategyConfigModel
 import space.dawdawich.strategy.model.CreateOrderFunction
@@ -8,7 +9,7 @@ import space.dawdawich.strategy.model.Order
 import space.dawdawich.strategy.model.Trend
 import space.dawdawich.utils.trimToStep
 import java.util.*
-
+val logger = KotlinLogging.logger {}
 fun getCreateOrderFunction(strategyConfig: StrategyConfigModel, marketService: PrivateHttpClient): CreateOrderFunction {
     val createOrderFunction: CreateOrderFunction = {
             inPrice: Double,
@@ -20,6 +21,7 @@ fun getCreateOrderFunction(strategyConfig: StrategyConfigModel, marketService: P
         ->
         val orderId = UUID.randomUUID().toString()
         val orderQty = qty.trimToStep(strategyConfig.minQtyStep)
+        logger.info { "Try to place order: inPrice - $inPrice, symbol - $orderSymbol, qty - $qty" }
         val isSuccess =
             runBlocking {
                 marketService.createOrder(
