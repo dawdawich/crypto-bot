d737bebbcc49b5a99475fef7e39c7c9b570e4d5c
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2024-07-15 22:08:49 +0300

    Simplify order creation conditional in CandleTailStrategyRunner
    The conditional for creating orders in CandleTailStrategyRunner has been simplified. The check for the position's trend being long, which was previously part of the main condition, has been moved inside the code block. This change increases readability and maintainability of the code.

--- strategy-lib/src/main/kotlin/space/dawdawich/strategy/strategies/CandleTailStrategyRunner.kt
+++ strategy-lib/src/main/kotlin/space/dawdawich/strategy/strategies/CandleTailStrategyRunner.kt
@@ -38,7 +38,7 @@ class CandleTailStrategyRunner(
             var moneyToUse = money - ((position?.getPositionValue() ?: 0.0) / multiplier)
 
             logger.debug { "Order creation info: body - $body, lowerShadow - $lowerShadow, upperShadow - $upperShadow, moneyToUse - $moneyToUse, money - $money" }
-            val order = if (lowerShadow != 0.0 && lowerShadow > upperShadow && (position?.trend?.equals(Trend.LONG) != false)) {
+            val order = if (lowerShadow != 0.0 && lowerShadow > upperShadow) {
 
                 moneyToUse = (if (position?.trend?.equals(Trend.LONG) != false) moneyToUse else money) * (lowerShadow / totalRange).coerceAtMost(0.7)
                 createOrderFunction(
