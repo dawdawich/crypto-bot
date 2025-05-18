b7a59860daf5e0584432d0a5e11e5d778a4ab721
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2025-05-18 14:56:55 +0200

    - Update env props

--- api-service/src/main/kotlin/space/dawdawich/controller/BacktestController.kt
+++ api-service/src/main/kotlin/space/dawdawich/controller/BacktestController.kt
@@ -1,6 +1,8 @@
 package space.dawdawich.controller
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
@@ -19,10 +21,10 @@ class BacktestController(
     private val requestStatusService: RequestStatusService,
 ) {
 
-    @GetMapping("/request/all")
+    @GetMapping("/request/all", produces = [APPLICATION_JSON_VALUE])
     fun getBacktestsRequests(authentication: Authentication): List<RequestStatusResponse> = requestStatusService.getRequestStatusesForAccountId(authentication.name).reversed()
 
-    @GetMapping("/request/{requestId}")
+    @GetMapping("/request/{requestId}", produces = [APPLICATION_JSON_VALUE])
     fun getBacktestsDetails(authentication: Authentication, @PathVariable("requestId") requestId: String): ResponseEntity<BacktestRequestResultsResponse?> {
         if (!requestStatusService.isRequestIdExistForAccount(requestId, authentication.name)) {
             return ResponseEntity.notFound().build()
@@ -31,7 +33,7 @@ class BacktestController(
         return ResponseEntity.ok(backtestService.getBackTestResults(requestId))
     }
 
-    @PostMapping
+    @PostMapping(consumes = [APPLICATION_JSON_VALUE], produces = [APPLICATION_JSON_VALUE])
     @ResponseStatus(HttpStatus.CREATED)
     fun createBacktest(@RequestBody request: BacktestRequest, authentication: Authentication): RequestIdResponse {
         val requestId = UUID.randomUUID().toString()
