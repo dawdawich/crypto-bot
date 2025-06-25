b48e3a1846dce798d2ca96d95dfff985d74c1a02
Artem.Bahniuk <artem.bahniuk@whaleapp.com>
2025-06-25 22:46:22 +0200

    - Change test running for static config but for all symbols

--- commons/src/main/kotlin/space/dawdawich/constants/RabbitConstants.kt
+++ commons/src/main/kotlin/space/dawdawich/constants/RabbitConstants.kt
@@ -2,3 +2,7 @@ package space.dawdawich.constants
 
 const val BACK_TEST_SERVICE = "back-test-service"
 const val PREDEFINED_BACK_TEST_SERVICE = "predefined-back-test-service"
+
+const val BYBIT_TICKER_TOPIC = "bybit_ticker"
+
+const val NEW_LISTING_SERVICE = "new-listing-service"
